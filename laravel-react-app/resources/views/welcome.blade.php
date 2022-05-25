21a72953760f991a37a2653af8ef525b5ecf74ab
Mustafa Kattan <73656862+MustafaKattan@users.noreply.github.com>
2022-05-25 02:49:10 +0200

    test component
    add compo

--- laravel-react-app/resources/views/welcome.blade.php
+++ laravel-react-app/resources/views/welcome.blade.php
@@ -21,11 +21,8 @@
         </style>
     </head>
     <body class="antialiased">
-    <!-- React root DOM -->
-    <div id="user">
-    </div>
 
-    <!-- React JS -->
+
 
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
@@ -41,6 +38,9 @@
                     @endauth
                 </div>
             @endif
+            <!-- React root DOM -->
+                <div id="user">
+                </div>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
@@ -134,7 +134,7 @@
                 </div>
             </div>
         </div>
+    <!-- React JS -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
     </body>
 </html>
