286ca61ab910e2380d3270cd5d697cb68ae31885
Ayham Alkastantini <32424203+Ayhamalkastantini@users.noreply.github.com>
2022-06-19 18:15:59 +0200

    Update UserFactory.php

--- laravel-react-app/database/factories/UserFactory.php
+++ laravel-react-app/database/factories/UserFactory.php
@@ -21,8 +21,8 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'role_id' => Role::factory(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role_id' => rand(1,3),
+            'password' => $this->faker->password(), // password
             'remember_token' => Str::random(10),
         ];
     }
